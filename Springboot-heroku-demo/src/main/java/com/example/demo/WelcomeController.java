6887cb145a5b4a633f5744b86d82c6e6e9b58706
gaduprasad <gaduprasad@INBAWN166712.ad.harman.com>
2022-05-10 12:45:59 +0530

    commit

---
+++ Springboot-heroku-demo/src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class WelcomeController {
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to SpringBoot Heroku demo";
+	}
+
+}
